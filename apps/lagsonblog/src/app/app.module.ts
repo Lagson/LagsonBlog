60f0a90bb25bf3a3afe13bd9088ed51dc0d56176
lagueu <lagson2@yahoo.fr>
2020-08-01 22:29:06 +0200

    added home feature library

--- apps/lagsonblog/src/app/app.module.ts
+++ apps/lagsonblog/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,7 +6,20 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path:'home-feature',
+        loadChildren: () =>
+        import('@lagson-inc/home/feature').then(
+          module => module.HomeFeatureModule
+        )
+      }
+    ],
+    {initialNavigation: 'enabled'}
+    )
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
