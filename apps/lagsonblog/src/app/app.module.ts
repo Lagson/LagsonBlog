5edfbe41fdcb8170ee7ae7fb9ae330fbc142ca60
lagueu <lagson2@yahoo.fr>
2020-10-22 22:21:30 +0200

    added angular material

--- apps/lagsonblog/src/app/app.module.ts
+++ apps/lagsonblog/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +19,8 @@ import { AppComponent } from './app.component';
       }
     ],
     {initialNavigation: 'enabled'}
-    )
+    ),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
